8e09cc42d51cf609a3ede09e56a6cb4653ac23a0
Py528 <pranaav.shinde5280@gmail.com>
2023-03-19 12:10:26 +0530

    moved components folder

--- first-app/src/statefull.jsx
+++
@@ -1,5 +0,0 @@
-class StateFull extends React.Component {
-	render() {
-		return <h1>StateFull {this.props.name}</h1>;
-	}
-}
--- first-app/src/stateless.jsx
+++
@@ -1,4 +0,0 @@
-import { React } from 'react';
-export function Stateless(props) {
-    return <h1>Hello World {props.name}</h1>;
-}
