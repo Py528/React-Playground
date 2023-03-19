4528a3c5099d47a81dd62c3b4334a34ce340e07b
Py528 <pranaav.shinde5280@gmail.com>
2023-03-19 12:12:13 +0530

    added different components from components folder

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import { Stateless } from './stateless';
-import { StateFull } from './stateless';
-
+import Stateless from './components/stateless';
+import StateFull from './components/statefull';
+import NoJSX from './components/NoJSX';
+import Message from './components/Message';
+import Counter from './components/Counter';
+import Quiz from './components/Quiz';
+import WholeScreenKey from './components/WholeScreenKey';
 function App() {
 	return (
 		<div className="App">
-			<Stateless />
-			<StateFull name="React" />
+			{/* <Stateless name="React" />
+			<StateFull name="React" /> */}
+			{/* <NoJSX /> */}
+			<Message />
+			<Counter />
+			<Quiz />
+			<WholeScreenKey />
 		</div>
 	);
 }
